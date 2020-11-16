2c83894e32ca4d7e1e23f7e22d775b8d8cb43a44
kazumaban <1310350007t@gmail.com>
2020-11-16 16:46:25 +0900

    forth commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,18 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :firstname_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :lastname_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
     validates :birthday
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' } do
+      validates :firstname
+      validates :lastname
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' } do
+      validates :firstname_katakana
+      validates :lastname_katakana
+    end
+
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: 'is invalid. Password Include both letters and numbers.'}
   end
 end
