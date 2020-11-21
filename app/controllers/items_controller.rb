d6277046d5134f80e1d9ecf85006a3b49808206e
kazumaban <1310350007t@gmail.com>
2020-11-21 16:51:35 +0900

    商品削除機能03

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,9 +38,9 @@ class ItemsController < ApplicationController
 
   def destroy
     if user_signed_in? && current_user.id == @item.user_id
-    @item.destroy
-    redirect_to root_path
+      @item.destroy
     end
+    redirect_to root_path
   end
 
   def move_to_index
